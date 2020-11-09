6471a1b1f3b07ed5f4bed652aad5809efe41bfc4
joedip16 <joedippolito16@gmail.com>
2020-11-09 09:40:34 -0500

    created variables

---
+++ newScript.js
@@ -0,0 +1,8 @@
+//Dom elements from html file
+var startBtn = document.getElementById("start");
+var startScreen = document.getElementById("start-screen");
+var questionsScreen = document.getElementById("question-screen");
+var gameOverScreen = document.getElementById("game-over-screen");
+var answers = []
+var index = 0
+var score = 0
