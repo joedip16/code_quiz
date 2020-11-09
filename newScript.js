eb9b9941aabfc6e7877aa9dd475f0c95a0e9f225
joedip16 <joedippolito16@gmail.com>
2020-11-09 09:44:34 -0500

    created questions array

--- newScript.js
+++ newScript.js
@@ -6,3 +6,26 @@ var gameOverScreen = document.getElementById("game-over-screen");
 var answers = []
 var index = 0
 var score = 0
+
+var questions = [
+    {
+        q: "What is the name of the Philadelphia NFL team?",
+        answers: ["Eagles", "Falcons", "Patriots", "Cardinals"],
+        correctAnswer: "Eagles"
+    },
+    {
+        q: "What is the name of the Philadelphia MLB team?",
+        answers: ["Red Sox", "Braves", "Phillies", "Tigers"],
+        correctAnswer: "Phillies"
+    },
+    {
+        q: "What is the name of the Philadelphia NBA team?",
+        answers: ["Suns", "Lakers", "Pistons", "76ers"],
+        correctAnswer: "76ers"
+    },
+    {
+        q: "What is the name of the Philadelphia NHL team?",
+        answers: ["Penguins", "Flyers", "Capitols", "Lightning"],
+        correctAnswer: "Flyers"
+    },
+];
